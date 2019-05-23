664725c5da753b9d19b4cb89ca3f2e211f405220
Phone Myat Khine <phonemyatkhine88@gmail.com>
2019-05-23 21:24:55 +0630

    23.5.2019

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,82 +4,115 @@ namespace App\Http\Controllers;
 
 use App\News;
 use Illuminate\Http\Request;
+use App\Events;
+use App\EventsPhotos;
+
 
 class NewsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+  public function index()
     {
-        //
+        $data = events::all();
+        $photo = eventsphotos::all();
+        return view('crud.events.index',compact('data','photo'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        $tags = tags::all();
+        $clubs = clubs::all();
+        return view('crud.events.create',compact('tags','clubs'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $events = new events;
+
+
+        $events->name = $request->name;
+        $events->description = $request->description;
+        $events->date = $request->date;
+        //$events->time = $request->start_time." to ".$request->end_time;
+        $events->time = "9:00";
+        $events->tags_id = $request->tags_id;
+        $events->clubs_id = $request->clubs_id;
+
+        $events->save();
+
+        $photo_count = $request->photo_count;
+        for ($i=0; $i <=$photo_count-1 ; $i++) {
+                    $events_photos = new EventsPhotos;
+                    if(Input::hasFile('photo')) {
+                        $files = Input::file('photo');
+                        $photo = $files[$i];
+                        $events_photos->events_id = $events->id;
+                        $photo->move('eventsPhotos', $photo->getClientOriginalName());
+                        $events_photos->photo = $photo->getClientOriginalName();
+                        $events_photos->save();
+                    } else {
+                        return back()->with('danger','Files Upload has errors.');
+                    }
+                }
+
+        return redirect('/events');
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\News  $news
-     * @return \Illuminate\Http\Response
-     */
-    public function show(News $news)
+    public function show($id)
     {
-        //
+
+        $events = events::where('id',$id)->first();
+        return view('crud.events.view',compact('events'));
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\News  $news
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(News $news)
+    public function edit($id)
     {
-        //
+
+        $events = events::where('id',$id)->first();
+         $committees = committees::all();
+        return view('crud.events.edit',compact('events','committees'));
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\News  $news
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, News $news)
+
+    public function update(Request $request, $id)
     {
-        //
+
+        $new_id = $request->id;
+        $name = $request->name;
+        $description = $request->description;
+        $founding_date = $request->founding_date;
+        $committees_id = $request->committees_id;
+        $logo = $request->logo;
+
+        $events = events::find($id);
+        if($new_id != $id) {
+            $events->id = $new_id;
+        }
+        if($logo != $events->logo) {
+            $image_path = 'eventsLogos/'.$events->logo;
+            if(File::exists($image_path)) {
+                File::delete($image_path);
+            }
+            if(Input::hasFile('logo')) {
+                $logo = Input::file('logo');
+                $logo->move('eventsLogos', $logo->getClientOriginalName());
+                $events->logo = $logo->getClientOriginalName();
+            }
+        }
+        $events->name = $name;
+        $events->description = $description;
+        $events->founding_date = $founding_date;
+        $events->committees_id = $committees_id;
+        $events->save();
+        return redirect('/events');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\News  $news
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(News $news)
+    public function destroy($id)
     {
-        //
+        $events = events::find($id);
+        $events->delete();
+        return redirect('/events');
     }
 }
